0acd6ac7f512ce9f7b4d2dfa98f303ba3d54c8ec
laith Alheo <146528836+Laith-Alhelo@users.noreply.github.com>
2025-11-16 00:58:12 +0400

    Add project files.

---
+++ LeaveManagement.Domain/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Domain
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string FullName { get; set; }
+        public string Position { get; set; }
+        public Manager Manager { get; set; }
+        public int ManagerId { get; set; }
+        public DateTime HireDate { get; set; }
+        public ICollection<WorkLeaveRequest> WorkLeaveRequests { get; set; }
+
+    }
+}
---
+++ LeaveManagement.Domain/WorkLeaveRequest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeaveManagement.Domain
+{
+    public class WorkLeaveRequest
+    {
+        public int Id { get; set; }
+        public Employee Employee { get; set; }
+        public int EmployeeId { get; set; }
+        public Manager Manager { get; set; }
+        public int Managerid { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Reason { get; set; }
+        public DateTime RequestDate { get; set; } = DateTime.Now;
+
+    }
+}
