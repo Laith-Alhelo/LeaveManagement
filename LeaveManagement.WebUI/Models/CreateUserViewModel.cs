e312a0dcec7c8456dc6fc994ac0fb8251ec78282
laith Alheo <146528836+Laith-Alhelo@users.noreply.github.com>
2025-11-17 13:34:37 +0400

    add create user and Readme file

---
+++ LeaveManagement.WebUI/Models/CreateUserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LeaveManagement.WebUI.Models
+{
+    public class CreateUserViewModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "Role")]
+        public string Role { get; set; } = "Employee"; 
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Temporary Password")]
+        public string Password { get; set; } = "Temp@12345";
+    }
+}
